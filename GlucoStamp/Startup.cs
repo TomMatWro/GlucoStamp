6559627a52fb8c40d62f3dd58179ed3a4c94f4da
Tomasz Matuszczak <tomasz.matuszczak@gmail.com>
2016-11-16 14:59:17 +0100

    MVC project created

---
+++ GlucoStamp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GlucoStamp.Startup))]
+namespace GlucoStamp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
